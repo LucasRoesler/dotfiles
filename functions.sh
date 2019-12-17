f731f871076e1888eab159c0c2146cda67b5a7d7
Lucas Roesler <roesler.lucas@gmail.com>
2019-12-17 10:17:29 +0100

    Remove unused function
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- functions.sh
+++
@@ -1,9 +0,0 @@
-function shell_into_docker () {
-    case "$1" in
-    "-b" | "--bash")
-        docker exec -it "$2" /bin/bash
-        ;;
-    *)
-        docker exec -it "$1" /bin/sh
-    esac
-}
