46fd347cdc9d72e7d3930794736992c7f10188f0
Lucas Roesler <roesler.lucas@gmail.com>
2021-10-25 14:33:40 +0200

    feat: add notification on sound output change
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- bin/toggle-sound-output.sh
+++ bin/toggle-sound-output.sh
@@ -1,36 +1,40 @@
-#!/bin/bash    
+#!/bin/bash
 # from https://askubuntu.com/a/1321904
 
-declare -i sinks_count=`pacmd list-sinks | grep -Pc 'index:\s+\d+'`    
-    
-if [ $sinks_count -eq 0 ] ; then    
-    exit    
-fi    
-    
-declare -i active_sink_index=`pacmd list-sinks | grep -Po '\*\s+index:\s+\K\d+'`    
-    
-active_index_position_found=0    
-let next_sink_index=-1    
-while read index ;    
-do    
-    declare -i ind=($(echo $index | tr -dc '[0-9]+'))    
-    if [ $next_sink_index -lt 0 ] ; then    
-        export next_sink_index=$ind    
-    fi    
-    if [ $active_index_position_found -eq 1 ] ; then    
-        export next_sink_index=$ind    
-        break;    
-    fi    
-    if [ $active_sink_index -eq $ind ] ; then    
-        export active_index_position_found=1    
-    fi    
-done < <(pacmd list-sinks | grep -Po 'index:\s+\K\d+')    
-    
-#change the default sink    
-pacmd "set-default-sink ${next_sink_index}"    
-    
+declare -i sinks_count=`pacmd list-sinks | grep -Pc 'index:\s+\d+'`
+
+if [ $sinks_count -eq 0 ] ; then
+    exit
+fi
+
+declare -i active_sink_index=`pacmd list-sinks | grep -Po '\*\s+index:\s+\K\d+'`
+
+active_index_position_found=0
+let next_sink_index=-1
+while read index ;
+do
+    declare -i ind=($(echo $index | tr -dc '[0-9]+'))
+    if [ $next_sink_index -lt 0 ] ; then
+        export next_sink_index=$ind
+    fi
+    if [ $active_index_position_found -eq 1 ] ; then
+        export next_sink_index=$ind
+        break;
+    fi
+    if [ $active_sink_index -eq $ind ] ; then
+        export active_index_position_found=1
+    fi
+done < <(pacmd list-sinks | grep -Po 'index:\s+\K\d+')
+
+#change the default sink
+pacmd "set-default-sink ${next_sink_index}"
+
 #move all inputs to the new sink
 for app in $(pacmd list-sink-inputs | grep -Po 'index:\s+\K\d+');
 do
     pacmd "move-sink-input $app $next_sink_index"
 done
+
+sink_name=$(pacmd list-sinks | grep -P "index:|\s+device\.description\s+=" | grep -A 1 -C 0  -P "\*\s+index" | tail -n 1 | sed -n 's/^.*"\(.*\)".*$/\1/p')
+
+notify-send -t 150 -c device -i multimedia-volume-control "Output changed" "Output set to $sink_name"
