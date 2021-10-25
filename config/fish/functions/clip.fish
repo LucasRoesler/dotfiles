2c1bb7626c204c5fc126c47cccd8921dbcf37137
Lucas Roesler <roesler.lucas@gmail.com>
2021-10-25 14:34:13 +0200

    feat: add wayland support to the clip function
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- config/fish/functions/clip.fish
+++ config/fish/functions/clip.fish
@@ -1,3 +1,9 @@
-function clip --wraps=xclip --description 'alias clip=xclip -sel c'
-  xclip -sel c $argv
+if [ "$XDG_SESSION_TYPE" = "wayland" ]
+  function clip --wraps=wl-copy --description 'alias clip=wl-copy'
+      wl-copy $argv
+  end
+else
+  function clip --wraps=xclip --description 'alias clip=xclip -sel c'
+      xclip -sel c $argv
+  end
 end
